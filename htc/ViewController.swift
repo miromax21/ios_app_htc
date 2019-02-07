8785ee96332c7446ea84b1961ea0c15cc19eca10
miroma21 <miromax21@gmail.com>
2019-02-07 19:12:57 +0400

    Initial Commit

---
+++ htc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  htc
+//
+//  Created by Princess Max on 07.02.2019.
+//  Copyright © 2019 Princess Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
